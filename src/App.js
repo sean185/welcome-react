2781b1daf79ad62e70847b40952f32c447325100
Sean Ang <seanang185@gmail.com>
2018-01-19 21:16:58 +0800

    added content from main scaffold

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Col, Row, Jumbotron, Button, Well } from 'react-bootstrap';
+
+import { TopNav, Footer } from './TopNav.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TopNav/>
+        <Grid>
+          <Row>
+            <Col>
+              <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                  This is a simple hero unit, a simple jumbotron-style component for calling
+                  extra attention to featured content or information.
+                </p>
+                <p>
+                  <Button bsStyle="primary">Learn more</Button>
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={6} md={4} xsOffset={3} mdOffset={1}>
+              <h2>Breadth</h2>
+              <p> I love learning new technologies, getting my hands dirty, and witness its potential for myself.</p>
+              <p>I am a unique problem solver due to a variety of experiences; ranging from leadership, to research.</p>
+            </Col>
+            <Col xs={6} md={4} xsOffset={6} mdOffset={1}>
+              <h2>Depth</h2>
+              <p> I love diving in deep into a topic, accumulating an unshakeable mental model of it.</p>
+              <p>I am a generalist, capable of being specialist when necessary, whilst melding cross-domain knowledge to aid in problem solving.</p>
+            </Col>
+          </Row>
+      
+          <Row className="show-grid">
+            <Col xs={6} md={4}>
+              <Well>Well,</Well>
+            </Col>
+            <Col xs={6} md={4}>
+              <Well>Well,</Well>
+            </Col>
+            <Col xsHidden md={4}>
+              <Well>Well,</Well>
+            </Col>
+          </Row>
+      
+          <Row className="show-grid">
+            <Col xs={6} xsOffset={6}>
+              <Well>Well,</Well>
+            </Col>
+          </Row>
+      
+          <Row className="show-grid">
+            <Col md={6} mdPush={6}>
+              <Well>Well,</Well>
+            </Col>
+            <Col md={6} mdPull={6}>
+              <Well>Well.</Well>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Footer/>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
---
+++ src/TopNav.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Jumbotron, Button, Nav, NavItem, Navbar, NavDropdown, MenuItem } from 'react-bootstrap';
+
+const navbarInstance = (
+	<Navbar inverse collapseOnSelect>
+		<Navbar.Header>
+			<Navbar.Brand>
+				<a href="#brand">React-Bootstrap</a>
+			</Navbar.Brand>
+			<Navbar.Toggle />
+		</Navbar.Header>
+		<Navbar.Collapse>
+			<Nav>
+				<NavItem eventKey={1} href="#">
+					Link
+		</NavItem>
+				<NavItem eventKey={2} href="#">
+					Link
+		</NavItem>
+				<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+					<MenuItem eventKey={3.1}>Action</MenuItem>
+					<MenuItem eventKey={3.2}>Another action</MenuItem>
+					<MenuItem eventKey={3.3}>Something else here</MenuItem>
+					<MenuItem divider />
+					<MenuItem eventKey={3.3}>Separated link</MenuItem>
+				</NavDropdown>
+			</Nav>
+			<Nav pullRight>
+				<NavItem eventKey={1} href="#">
+					Link Right
+		</NavItem>
+				<NavItem eventKey={2} href="#">
+					Link Right
+		</NavItem>
+			</Nav>
+		</Navbar.Collapse>
+	</Navbar>
+);
+class TopNav extends Component {
+	render() {
+		return navbarInstance;
+	}
+}
+
+const footerInstance = (
+	<Jumbotron>
+		<h1>Footer!</h1>
+		<p>
+			This is where the footer will be
+	</p>
+
+		<Nav bsStyle="pills" stacked activeKey={1}>
+			<NavItem eventKey={1} href="/home">
+				NavItem 1 content
+	  </NavItem>
+			<NavItem eventKey={2} title="Item">
+				NavItem 2 content
+	  </NavItem>
+			<NavItem eventKey={3} disabled>
+				NavItem 3 content
+	  </NavItem>
+		</Nav>
+
+		<p>
+			<Button bsStyle="primary">Learn more</Button>
+		</p>
+	</Jumbotron>
+);
+
+class Footer extends Component {
+	render() {
+		return footerInstance;
+	}
+}
+
+export { TopNav, Footer };
